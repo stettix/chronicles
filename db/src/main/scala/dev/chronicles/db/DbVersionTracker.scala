e4d4f200ddc7ad5bf9ea17249bae6e36a439f667
stettix <jan.stette@gmail.com>
2019-10-01 20:25:53 +0100

    Tidy up

--- db/src/main/scala/dev/chronicles/db/DbVersionTracker.scala
+++ db/src/main/scala/dev/chronicles/db/DbVersionTracker.scala
@@ -10,7 +10,7 @@ import dev.chronicles.core.VersionTracker._
 import dev.chronicles.core._
 import doobie._
 import doobie.implicits._
-import fs2.{Chunk, Stream}
+import fs2.Stream
 
 /**
   * This stores version history in a JDBC compatible database.
@@ -18,66 +18,24 @@ import fs2.{Chunk, Stream}
 class DbVersionTracker[F[_]](xa: Transactor[F])(implicit cs: ContextShift[F], F: Bracket[F, Throwable])
     extends VersionTracker[F] {
 
+  import DbVersionTracker._
+
   /**
     * Create table schemas if not already present.
     */
   def init(): F[Unit] = {
-    val createTablesTable =
-      sql"""create table if not exists `chronicle_tables_v1` (
-           |  metastore_id                varchar(32),
-           |  table_name                  varchar(512),
-           |  init_commit_id              varchar(36),
-           |  creation_time               timestamp not null,
-           |  created_by                  varchar(32) not null,
-           |  message                     varchar(4096) not null,
-           |  is_snapshot_table           boolean not null,
-           |  primary key (metastore_id, table_name)
-           |)
-           |""".stripMargin.update.run
 
-    val createUpdatesTable =
-      sql"""create table if not exists `chronicle_table_updates_v1` (
-           |  sequence_id                 long generated always as identity,
-           |  commit_id                   varchar(36),
-           |  metastore_id                varchar(32),
-           |  table_name                  varchar(512),
-           |  update_time                 timestamp not null,
-           |  user_id                     varchar(32) not null,
-           |  message                     varchar(4096) not null,
-           |  primary key (commit_id),
-           |  constraint fk_chronicle_table_updates_v1_to_tables foreign key (metastore_id, table_name) references chronicle_tables_v1(metastore_id, table_name)
-           |)
-           |""".stripMargin.update.run
-
-    val createOperationsTable =
-      sql"""create table if not exists `chronicle_table_operations_v1` (
-           |  commit_id                   varchar(36),
-           |  index_in_commit             int not null,
-           |  operation_type              enum('update_table_version', 'add_part_version', 'remove_part') not null,
-           |  version                     varchar(62) not null,
-           |  partition                   varchar(1024) not null,
-           |  primary key (commit_id),
-           |  constraint fk_chronicle_table_operations_v1_to_updates foreign key (commit_id) references chronicle_table_updates_v1(commit_id)
-           |)
-           |""".stripMargin.update.run
-
-    val createVersionRefsTable =
-      sql"""create table if not exists `chronicles_version_refs_v1` (
-           |  metastore_id                varchar(32),
-           |  table_name                  varchar(512),
-           |  current_version             varchar(62) not null,
-           |  constraint fk_chronicles_version_refs_v1_to_table foreign key (metastore_id, table_name) references chronicle_tables_v1(metastore_id, table_name)
-           |)
-           |""".stripMargin.update.run
-
-    val createTables = createTablesTable >> createUpdatesTable >> createOperationsTable >> createVersionRefsTable
+    val createTables =
+      createTablesTable.run *>
+        createUpdatesTable.run *>
+        createOperationsTable.run *>
+        createVersionRefsTable.run
 
     createTables.transact(xa).void
   }
 
   override def tables(): F[List[TableName]] = {
     val rawTableNames = DbVersionTracker.allTablesQuery
-      .query[String]
       .to[List]
       .transact(xa)
 
@@ -105,7 +63,7 @@ class DbVersionTracker[F[_]](xa: Transactor[F])(implicit cs: ContextShift[F], F:
 
     // Add table if it doesn't exist already
     val addIfNotExists = for {
-      existingCount <- DbVersionTracker.tableExistsQuery(table)
+      existingCount <- DbVersionTracker.tableCountQuery(table).unique
       _ <- if (existingCount == 0) addTableQuery else Unit.pure[ConnectionIO]
     } yield ()
 
@@ -116,15 +74,8 @@ class DbVersionTracker[F[_]](xa: Transactor[F])(implicit cs: ContextShift[F], F:
     // First get the metadata for the 'init' operation, by querying table metadata.
     // Make this query produce an error if the table isn't found, using the 'unique' operation on the query.
 
-    def tableMetadataQuery(table: TableName): doobie.ConnectionIO[(String, Instant, String, String, Boolean)] =
-      sql"""
-           |select init_commit_id, creation_time, created_by, message, is_snapshot_table
-           |  from chronicle_tables_v1
-           |  where table_name = ${table.fullyQualifiedName}
-           |""".stripMargin.query[(String, Instant, String, String, Boolean)].unique
-
     val initialTableUpdate: ConnectionIO[TableUpdate] = for {
-      (initCommitId, creationTime, createdBy, message, isSnapshot) <- tableMetadataQuery(table)
+      (initCommitId, creationTime, createdBy, message, isSnapshot) <- tableMetadataQuery(table).unique
       metadata = TableUpdateMetadata(CommitId(initCommitId), UserId(createdBy), UpdateMessage(message), creationTime)
     } yield TableUpdate(metadata, operations = List(InitTable(table, isSnapshot)))
 
@@ -147,22 +98,8 @@ class DbVersionTracker[F[_]](xa: Transactor[F])(implicit cs: ContextShift[F], F:
         metadata = TableUpdateMetadata(CommitId(commitId), UserId(createdBy), UpdateMessage(message), creationTime)
       } yield TableUpdate(metadata, operations = List(operation))
 
-    val updatesQuery =
-      sql"""select
-      |       u.commit_id, u.update_time, u.user_id, u.message,
-      |       o.operation_type, o.version, o.partition
-      |     from chronicle_tables_v1 t
-      |       inner join chronicle_table_updates_v1 u
-      |     on t.metastore_id = u.metastore_id and t.table_name = u.table_name
-      |       inner join chronicle_table_operations_v1 o
-      |     on u.commit_id = o.commit_id
-      |     where t.table_name = ${table.fullyQualifiedName}
-      |     order by u.sequence_id, o.index_in_commit
-      |""".stripMargin
-        .query[(String, Instant, String, String, String, String, String)]
-
     val updatesStream: Stream[doobie.ConnectionIO, (TableUpdateMetadata, List[TableOperation])] =
-      updatesQuery.stream
+      updatesQuery(table).stream
         .map((toUpdate _).tupled)
         .flatMap(Stream.fromEither[ConnectionIO](_))
         .groupAdjacentBy(_.metadata)(Eq.fromUniversalEquals[TableUpdateMetadata])
@@ -205,12 +142,81 @@ class DbVersionTracker[F[_]](xa: Transactor[F])(implicit cs: ContextShift[F], F:
 
 object DbVersionTracker {
 
+  val createTablesTable =
+    sql"""create table if not exists `chronicle_tables_v1` (
+         |  metastore_id                varchar(32),
+         |  table_name                  varchar(512),
+         |  init_commit_id              varchar(36),
+         |  creation_time               timestamp not null,
+         |  created_by                  varchar(32) not null,
+         |  message                     varchar(4096) not null,
+         |  is_snapshot_table           boolean not null,
+         |  primary key (metastore_id, table_name)
+         |)
+         |""".stripMargin.update
+
+  val createUpdatesTable =
+    sql"""create table if not exists `chronicle_table_updates_v1` (
+         |  sequence_id                 long generated always as identity,
+         |  commit_id                   varchar(36),
+         |  metastore_id                varchar(32),
+         |  table_name                  varchar(512),
+         |  update_time                 timestamp not null,
+         |  user_id                     varchar(32) not null,
+         |  message                     varchar(4096) not null,
+         |  primary key (commit_id),
+         |  constraint fk_chronicle_table_updates_v1_to_tables foreign key (metastore_id, table_name) references chronicle_tables_v1(metastore_id, table_name)
+         |)
+         |""".stripMargin.update
+
+  val createOperationsTable =
+    sql"""create table if not exists `chronicle_table_operations_v1` (
+         |  commit_id                   varchar(36),
+         |  index_in_commit             int not null,
+         |  operation_type              enum('update_table_version', 'add_part_version', 'remove_part') not null,
+         |  version                     varchar(62) not null,
+         |  partition                   varchar(1024) not null,
+         |  primary key (commit_id),
+         |  constraint fk_chronicle_table_operations_v1_to_updates foreign key (commit_id) references chronicle_table_updates_v1(commit_id)
+         |)
+         |""".stripMargin.update
+
+  val createVersionRefsTable =
+    sql"""create table if not exists `chronicles_version_refs_v1` (
+         |  metastore_id                varchar(32),
+         |  table_name                  varchar(512),
+         |  current_version             varchar(62) not null,
+         |  constraint fk_chronicles_version_refs_v1_to_table foreign key (metastore_id, table_name) references chronicle_tables_v1(metastore_id, table_name)
+         |)
+         |""".stripMargin.update
+
   val allTablesQuery =
     sql"""select table_name from `chronicle_tables_v1`"""
+      .query[String]
 
-  def tableExistsQuery(table: TableName) =
+  def tableCountQuery(table: TableName) =
     sql"""select count(*) from `chronicle_tables_v1` where table_name = ${table.fullyQualifiedName}"""
       .query[Long]
-      .unique
+
+  def tableMetadataQuery(table: TableName) =
+    sql"""
+         |select init_commit_id, creation_time, created_by, message, is_snapshot_table
+         |  from chronicle_tables_v1
+         |  where table_name = ${table.fullyQualifiedName}
+         |""".stripMargin.query[(String, Instant, String, String, Boolean)]
+
+  def updatesQuery(table: TableName) =
+    sql"""select
+         |       u.commit_id, u.update_time, u.user_id, u.message,
+         |       o.operation_type, o.version, o.partition
+         |     from chronicle_tables_v1 t
+         |       inner join chronicle_table_updates_v1 u
+         |     on t.metastore_id = u.metastore_id and t.table_name = u.table_name
+         |       inner join chronicle_table_operations_v1 o
+         |     on u.commit_id = o.commit_id
+         |     where t.table_name = ${table.fullyQualifiedName}
+         |     order by u.sequence_id, o.index_in_commit
+         |""".stripMargin
+      .query[(String, Instant, String, String, String, String, String)]
 
 }
