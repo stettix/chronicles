1d42dccaa1625030ce7467e5953621c7594426e3
stettix <jan.stette@gmail.com>
2019-10-03 10:52:35 +0100

    Implement remaining operations
    Functional tests pass now, but some work remains in handling error cases, producing the right error messages as necessary.

--- db/src/it/scala/dev/chronicles/db/DbVersionTrackerIntegrationTest.scala
+++ db/src/it/scala/dev/chronicles/db/DbVersionTrackerIntegrationTest.scala
@@ -1,7 +1,10 @@
 package dev.chronicles.db
 
+import java.time.Instant
+
 import cats.effect._
 import dev.chronicles.core.TableName
+import dev.chronicles.core.VersionTracker._
 import doobie.util.transactor.Transactor
 import doobie.util.transactor.Transactor.Aux
 import org.scalatest.{FlatSpec, Matchers}
@@ -24,5 +27,16 @@ class DbVersionTrackerIntegrationTest extends FlatSpec with Matchers with doobie
   "tableCountQuery" should "be valid" in { check(DbVersionTracker.tableCountQuery(table)) }
   "tableMetadataQuery" should "be valid" in { check(DbVersionTracker.tableMetadataQuery(table)) }
   "updatesQuery" should "be valid" in { check(DbVersionTracker.updatesQuery(table)) }
+  "addTableQuery" should "be valid" in {
+    check(DbVersionTracker
+      .addTableUpdate(table, CommitId("id"), Instant.now(), UserId("id"), UpdateMessage("message"), isSnapshot = false))
+  }
+  "currentVersionQuery" should "be valid" in { check(DbVersionTracker.currentVersionQuery(table)) }
+  "setCurrentVersionUpdate" should "be valid" in {
+    check(DbVersionTracker.initialiseCurrentVersionUpdate(table, CommitId("id")))
+  }
+  "updateCurrentVersionUpdate" should "be valid" in {
+    check(DbVersionTracker.updateCurrentVersionUpdate(table, CommitId("id")))
+  }
 
 }
