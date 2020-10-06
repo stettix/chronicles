62704cb9dd6c3ad7fa1147dadf1695327a46b9a0
Jan Stette <jan.stette@gmail.com>
2020-10-06 11:28:56 +0100

    Fix tests

--- filebacked/src/test/scala/dev/chronicles/filebacked/TimestampsSpec.scala
+++ filebacked/src/test/scala/dev/chronicles/filebacked/TimestampsSpec.scala
@@ -5,32 +5,15 @@ import cats.effect._
 
 class TimestampsSpec extends FlatSpec with Matchers {
 
-  val testStream1 = Timestamps.distinctTimestamps[IO].take(1000)
+  val testStream = Timestamps.uniqueTimestamps[IO].take(1000)
 
   "A stream of timestamps" should "have all distinct values" in {
-    val values = testStream1.compile.toVector.unsafeRunSync()
+    val values = testStream.compile.toVector.unsafeRunSync()
     values.distinct should contain theSameElementsInOrderAs values
   }
 
   it should "ensure subsequent timestamps are equal or in increasing order" in {
-    val timestampPairs = testStream2.zipWithPrevious.compile.toVector.unsafeRunSync()
-
-    timestampPairs.foreach {
-      case (Some(prev), next) => assert(!prev.isAfter(next))
-      case (None, _)          =>
-    }
-
-  }
-
-  val testStream2 = Timestamps.uniqueTimestamps[IO].take(1000)
-
-  "A stream of timestamps" should "have all distinct values" in {
-    val values = testStream2.compile.toVector.unsafeRunSync()
-    values.distinct should contain theSameElementsInOrderAs values
-  }
-
-  it should "ensure subsequent timestamps are equal or in increasing order" in {
-    val timestampPairs = testStream2.zipWithPrevious.compile.toVector.unsafeRunSync()
+    val timestampPairs = testStream.zipWithPrevious.compile.toVector.unsafeRunSync()
 
     timestampPairs.foreach {
       case (Some(prev), next) => assert(!prev.isAfter(next))
