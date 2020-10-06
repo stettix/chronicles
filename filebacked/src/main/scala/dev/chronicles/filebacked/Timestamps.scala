e74176ad1aa3d2b2d094b28d58b01ababe5d26d2
Jan Stette <jan.stette@gmail.com>
2020-10-06 11:16:33 +0100

    Alternative implementations of timestamps

--- filebacked/src/main/scala/dev/chronicles/filebacked/Timestamps.scala
+++ filebacked/src/main/scala/dev/chronicles/filebacked/Timestamps.scala
@@ -2,6 +2,7 @@ package dev.chronicles.filebacked
 
 import java.time.Instant
 
+import cats.Eq
 import cats.effect._
 import cats.implicits._
 import fs2.Stream
@@ -22,12 +23,35 @@ object Timestamps {
 
     val timestampsWithDiscriminator: Stream[F, (Long, Int)] = timestamps.groupAdjacentBy(identity).flatMap {
       case (_, ts) if ts.size > 1 => Stream.emits(ts.toList.zipWithIndex)
-      case (t, _)                  => Stream.emit(t -> 0)
+      case (t, _)                 => Stream.emit(t -> 0)
     }
 
     timestampsWithDiscriminator.map {
       case (ts, idx) => Instant.ofEpochMilli(ts) -> (if (idx == 0) None else Some(idx))
     }
   }
+
+  def distinctTimestamps[F[_]](implicit F: Sync[F]): Stream[F, Instant] = {
+
+    def next(prev: Long): F[Option[(Long, Long)]] =
+      for {
+        tsMillis <- F.delay(System.currentTimeMillis())
+        next = if (prev < tsMillis) tsMillis else prev + 1
+      } yield Some(next -> next)
+
+    Stream
+      .unfoldEval(0L)(next)
+      .map(Instant.ofEpochMilli)
+  }
+
+  def uniqueInstants[F[_]](implicit F: Sync[F]): Stream[F, Instant] =
+    Stream.eval(F.delay(Instant.now())).repeat.changes(Eq.fromUniversalEquals)
+
+}
+
+object Foo extends App {
+
+  val out = Timestamps.uniqueInstants[IO].take(10).compile.toVector.unsafeRunSync()
+  out.foreach(println)
 
 }
--- filebacked/src/test/scala/dev/chronicles/filebacked/TimestampsSpec.scala
+++ filebacked/src/test/scala/dev/chronicles/filebacked/TimestampsSpec.scala
@@ -1,21 +1,36 @@
 package dev.chronicles.filebacked
 
-import java.time.Instant
-
 import org.scalatest.{FlatSpec, Matchers}
 import cats.effect._
 
 class TimestampsSpec extends FlatSpec with Matchers {
 
-  val testStream = Timestamps.uniqueTimestamps[IO].take(1000)
+  val testStream1 = Timestamps.distinctTimestamps[IO].take(1000)
 
   "A stream of timestamps" should "have all distinct values" in {
-    val values = testStream.compile.toVector.unsafeRunSync()
+    val values = testStream1.compile.toVector.unsafeRunSync()
     values.distinct should contain theSameElementsInOrderAs values
   }
 
   it should "ensure subsequent timestamps are equal or in increasing order" in {
-    val timestampPairs = testStream.map(_._1).zipWithPrevious.compile.toVector.unsafeRunSync()
+    val timestampPairs = testStream2.zipWithPrevious.compile.toVector.unsafeRunSync()
+
+    timestampPairs.foreach {
+      case (Some(prev), next) => assert(!prev.isAfter(next))
+      case (None, _)          =>
+    }
+
+  }
+
+  val testStream2 = Timestamps.uniqueInstants[IO].take(1000)
+
+  "A stream of timestamps" should "have all distinct values" in {
+    val values = testStream2.compile.toVector.unsafeRunSync()
+    values.distinct should contain theSameElementsInOrderAs values
+  }
+
+  it should "ensure subsequent timestamps are equal or in increasing order" in {
+    val timestampPairs = testStream2.zipWithPrevious.compile.toVector.unsafeRunSync()
 
     timestampPairs.foreach {
       case (Some(prev), next) => assert(!prev.isAfter(next))
